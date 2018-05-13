566fead4c60ab18e0d461f8f07e60e5591d7ca77
stop2stare <emitremmus0235@163.com>
2018-05-13 12:03:49 +0800

    build: build bundles

---
+++ dist/kill-null.js
@@ -0,0 +1,90 @@
+/**
+ * kill-null v0.0.1
+ * (c) 2018 Ryan Liu
+ * @license MIT
+ */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.kn = {})));
+}(this, (function (exports) { 'use strict';
+
+  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+  /**
+   * kill null if needed
+   * @param  {Object|Array} raw
+   * @param  {Object} types
+   * @return {Object|Array}
+   */
+  var killer = function killer(raw, types) {
+    if (types === undefined) return raw;
+    var copy = JSON.parse(JSON.stringify(raw));
+    walk(copy, types);
+    return copy;
+  };
+
+  // dictionary for default value of types
+  var typeDict = {
+    number: 0,
+    array: [],
+    object: {},
+    string: ""
+  };
+
+  var walk = function walk(raw, types) {
+    if (raw instanceof Array) {
+      for (var i = 0; i < raw.length; i++) {
+        doWalk(raw[i], types);
+      }
+    } else if ((typeof raw === "undefined" ? "undefined" : _typeof(raw)) === object) {
+      doWalk(raw, types);
+    }
+  };
+  /**
+   * walk types and check if raw data is as expected
+   * @param  {Object|Array} raw
+   * @param  {Object} types
+   */
+  var doWalk = function doWalk(raw, types) {
+    var keys = Object.keys(types);
+    var l = keys.length;
+    var key = void 0,
+        value = void 0,
+        type = void 0,
+        config = void 0;
+    while (l--) {
+      key = keys[l];
+      value = raw[key];
+      type = types[key];
+
+      config = (typeof type === "undefined" ? "undefined" : _typeof(type)) === object ? type : { type: type };
+      var _config = config,
+          requiredType = _config.type,
+          defaultValue = _config.default,
+          data = _config.data;
+
+
+      if (value === null) {
+        raw[key] = defaultValue ? defaultValue : typeDict[requiredType];
+      } else if (data !== undefined) {
+        walk(value, data);
+      }
+    }
+  };
+
+  // export types
+  var array = "array";
+  var object = "object";
+  var string = "string";
+  var number = "number";
+
+  exports.array = array;
+  exports.object = object;
+  exports.string = string;
+  exports.number = number;
+  exports.default = killer;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
